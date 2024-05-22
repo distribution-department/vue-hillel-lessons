6af28d07e10819e143934a687e66b81b973ac311
Stanislav <evgenija.khamrik@gmail.com>
2024-05-22 22:58:49 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function calculation(a, b) {
+  return a + b;
+}
+
+function subtraction(a, b) {
+  return a - b;
+}
+
+function multiplication(a, b) {
+  return a * b;
+}
+
+console.log(calculation(54, 17));
+console.log(subtraction(23, 9));
+console.log(multiplication(5, 32));
